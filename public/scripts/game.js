49b6ce2882a89b354ea78f6cf03fe5f90a52cee8
Lilian Padilla <lpadilla@trinity.edu>
2025-04-04 12:24:21 -0500

    character descriptions and game page update

---
+++ public/scripts/game.js
@@ -0,0 +1,41 @@
+
+// Script for User Interaction in Circles
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Get elements
+    const circles = document.querySelectorAll(".social-circle");
+    const overlay = document.getElementById("actionOverlay");
+    const closeOverlay = document.getElementById("closeOverlay");
+    
+    // Show open modal when a Social Circle is clicked
+    circles.forEach(circle => {
+        circle.addEventListener("click", () => {
+            overlay.style.display = "flex";
+        });
+    });
+    
+    // Hide overlay when the close button is clicked
+    closeOverlay.addEventListener("click", () => {
+        overlay.style.display = "none";
+        });
+    });
+    
+    // Exit Button to Return to Home Page
+    const exitBtn = document.querySelector(".exit-btn");
+    const modal = document.getElementById("exitModal");
+    const confirmExit = document.getElementById("confirmExit");
+    const cancelExit = document.getElementById("cancelExit");
+    
+    exitBtn.addEventListener('click', () => {
+        modal.style.display = "block";
+    });
+    
+    confirmExit.addEventListener("click", () => {
+        window.location.href = "homepage.html";
+    });
+    
+    cancelExit.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+    
+    
