8769fb514ec2e397e1ddf62e78fbff87550c93bd
Lilian Padilla <lpadilla@trinity.edu>
2025-04-21 21:43:18 -0500

    fixing query issues

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'd@taBAS3S17',
+    password: 'Trinity011204',
     database: 'v2SocialCircles'
 });
 
